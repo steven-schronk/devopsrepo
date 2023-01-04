14801dd68178e0a1316f02e7a4fe4762dec3abab
Steven Schronk <steven.schronk@coreavi.com>
2023-01-04 08:53:46 -0600

    Updated build script.

--- Project1/nasm_build.bat
+++ Project1/nasm_build.bat
@@ -1,3 +1,4 @@
-Project1\nasm\nasm -f win32 -o hello.o hello.asm
-Project1\nasm\ld -o hello.exe hello.o
+cd Project1
+nasm\nasm -f win32 -o hello.o hello.asm
+nasm\ld -o hello.exe hello.o
 hello.exe
