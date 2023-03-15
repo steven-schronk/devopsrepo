29e6534324210fc82b997cf683c68c82a93f9ed9
Steven Schronk <steven@schronk.com>
2023-03-15 16:21:59 -0500

    Update build.bat

--- Project1/build.bat
+++ Project1/build.bat
@@ -1,5 +1,12 @@
 %echo on
-
+del lex.yy.c
+del lex.yy.obj
+del model_parser.output
+del model_parser.tab.c
+del model_parser.tab.exe
+del model_parser.tab.h
+del model_parser.tab.h
+del model_parser.tab.obj
 bison -v --debug -d model_parser.y
 flex model_parser.l
 cl model_parser.tab.c lex.yy.c
